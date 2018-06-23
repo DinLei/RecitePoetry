0825ae0958a299f5819920df0fea76eb1985b590
dinglei <dinglei@banggood.cn>
2018-06-23 18:01:01 +0800

    编写语言模型

--- ml_tools/language_model.py
+++ ml_tools/language_model.py
@@ -13,7 +13,10 @@ class LanguageModel:
         self.__unigram = None
         self.__prob_model = None
 
-    def train(self, data):
+    def train(self, development_data):
+        pass
+
+    def load_corpus(self, corpus):
         pass
 
     def predict(self, sent):
@@ -28,34 +31,57 @@ class LanguageModel:
 
 class TrigramModel(LanguageModel):
     """
-    三元语法模型，使用二元、一元插值、折减平滑处理
+    三元语法模型，使用二元、一元插值平滑处理
     *: 首部填充值，代表开始
     $: 尾部填充值，代表结束
     """
     def __init__(self):
         self.__bigram = None
         self.__trigram = None
-        # super(TrigramModel, self).__init__()
         LanguageModel.__init__(self)
+        # super(TrigramModel, self).__init__()
 
-    def __init_single_model(self):
+    def __clean_up_single_model(self):
         if not self.__unigram:
-            self.__unigram = {"total_num": 0}
+            self.__unigram = {"total_num": 0, "grams": dict()}
+        else:
+            self.__unigram["total_num"] += sum(self.__unigram["grams"].values())
         if not self.__bigram:
-            self.__bigram = {"total_num": 0}
+            self.__bigram = {"total_num": 0, "grams": dict()}
+        else:
+            self.__bigram["total_num"] += sum(self.__bigram["grams"].values())
         if not self.__trigram:
-            self.__trigram = {"total_num": 0}
+            self.__trigram = {"total_num": 0, "grams": dict()}
+        else:
+            self.__trigram["total_num"] += sum(self.__trigram["grams"].values())
 
-    def train(self, data):
+    def load_corpus(self, corpus):
         """
         加载训练数据，构造模型
-        :param data: 句子的集合
+        :param corpus: 句子的集合，构造ngram的语料
         :return: void
         """
-        self.__init_single_model()
-        for sent in data:
+        self.__clean_up_single_model()
+        for sent in corpus:
             arr = list(sent)
             arr.insert(0, "*")
             arr.insert(0, "*")
             arr.append("$")
+            for ind in range(len(arr)):
+                if ind < len(arr) - 2:
+                    token3 = "".join(arr[ind: ind+3])
+                    if token3 not in self.__trigram["grams"]:
+                        self.__trigram["grams"][token3] = 0
+                    self.__trigram["grams"][token3] += 1
+                if 1 < ind < len(arr) - 1:
+                    token1 = arr[ind]
+                    if token1 not in self.__unigram["grams"]:
+                        self.__unigram["grams"][token1] = 0
+                    self.__unigram["grams"][token1] += 1
+                if 0 < ind < len(arr):
+                    token2 = "".join(arr[ind: ind + 2])
+                    if token2 not in self.__bigram["grams"]:
+                        self.__bigram["grams"][token2] = 0
+                    self.__bigram["grams"][token2] += 1
+        self.__clean_up_single_model()
     pass
